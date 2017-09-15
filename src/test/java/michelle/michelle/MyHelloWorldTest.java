14ef01d1adecee32c7b26f97dab4b41ba0df488a
Michelle <chelleseattle@gmail.com>
2017-09-14 21:43:56 -0700

    first commit

---
+++ src/test/java/michelle/michelle/MyHelloWorldTest.java
@@ -0,0 +1,13 @@
+package michelle.michelle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyHelloWorldTest {
+
+    @Test
+    public void testTheStuff() {
+        assertEquals(1,1);
+    }
+}
